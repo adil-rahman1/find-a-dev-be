a8a890d536d903247fda9c6cb8f6fe50c5dec2e5
Adil Rahman <82183381+adil-rahman1@users.noreply.github.com>
2024-12-31 12:41:24 +0000

    Added GET, POST and PATCH route handlers for /developers/:id/social-links endpoint

--- src/server.ts
+++ src/server.ts
@@ -21,6 +21,8 @@ app.get("/api", (_req, res) => {
   res.sendFile(pathToFile);
 });
 
+//Route handlers for /developers/
+
 app.get("/developers", async (_req, res) => {
   try {
     const sqlQuery = "SELECT * FROM developers ORDER BY id";
@@ -94,6 +96,106 @@ app.patch("/developers/:id", async (req, res) => {
   } catch (error) {
     console.error("Error running query:", error);
     res.status(500).send("Error updating developer");
+  }
+});
+
+//Route handlers for /developers/:id/social-links
+
+app.get("/developers/:id/social-links", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const sqlQuery = "SELECT * FROM social_links WHERE developer_id = $1";
+    const queryResult = await client.query(sqlQuery, [id]);
+    if (queryResult.rows.length === 0) {
+      res.status(404).json({ error: "Developer's social links not found" });
+    }
+    res.status(200).json(queryResult.rows[0]);
+  } catch (error) {
+    console.error("Error running query:", error);
+    res.status(500).send("Database query error");
+  }
+});
+
+app.post("/developers/:id/social-links", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const { linkedin, github, website, other } = req.body;
+
+    const insertClauses = ["developer_id"];
+    const queryValues = [id];
+    const placeholders = ["$1"];
+
+    if (linkedin) {
+      insertClauses.push("linkedin");
+      queryValues.push(linkedin);
+      placeholders.push(`$${queryValues.length}`);
+    }
+
+    if (github) {
+      insertClauses.push("github");
+      queryValues.push(github);
+      placeholders.push(`$${queryValues.length}`);
+    }
+
+    if (website) {
+      insertClauses.push("website");
+      queryValues.push(website);
+      placeholders.push(`$${queryValues.length}`);
+    }
+
+    if (other) {
+      insertClauses.push("other");
+      queryValues.push(other);
+      placeholders.push(`$${queryValues.length}`);
+    }
+
+    const sqlQuery = `INSERT INTO social_links (${insertClauses.join(", ")}) values (${placeholders.join(", ")}) returning *`;
+    console.log(sqlQuery);
+    console.log(queryValues);
+    const queryResult = await client.query(sqlQuery, queryValues);
+    res.status(201).json(queryResult.rows[0]);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Error creating social links" });
+  }
+});
+
+app.patch("/developers/:id/social-links", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const { linkedin, github, website, other } = req.body;
+
+    const setClauses = [];
+    const queryValues = [id];
+
+    if (linkedin) {
+      setClauses.push(`linkedin = $${queryValues.length + 1}`);
+      queryValues.push(linkedin);
+    }
+
+    if (github) {
+      setClauses.push(`github = $${queryValues.length + 1}`);
+      queryValues.push(github);
+    }
+
+    if (website) {
+      setClauses.push(`website = $${queryValues.length + 1}`);
+      queryValues.push(website);
+    }
+
+    if (other) {
+      setClauses.push(`other = $${queryValues.length + 1}`);
+      queryValues.push(other);
+    }
+
+    const sqlQuery = `UPDATE social_links SET ${setClauses.join(", ")} WHERE developer_id = $1 returning *`;
+    console.log(sqlQuery);
+    console.log(queryValues);
+    const queryResult = await client.query(sqlQuery, queryValues);
+    res.status(200).json(queryResult.rows[0]);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Error updating social links" });
   }
 });
 
